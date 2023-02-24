4e1c353fa7c753675ceb680f2f3f7db0e47bcee8
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2023-02-24 16:40:52 +0900

    Update decorator_example.py

--- adding_Authorization/decorator_example.py
+++ adding_Authorization/decorator_example.py
@@ -2,12 +2,15 @@ from functools import wraps
 
 def test_decorator(f): 
     @wraps(f) 
-    def decorated_function(*args, **kwargs):
+    def decorated_function(*args, **kwargs): #가능한 모든 인자를 받기 위해 *args, **kwargs
         print("Decorated Function")
         return f(*args, **kwargs)
 
     return decorated_function
 
-@test_decorator
+@test_decorator #데코레이터 함수 지정하는 법
 def func():
     print("Calling func function")
+
+#이렇게 코드를 작성 시 실행순서는 
+# decorated_fucntion > func 순으로 실행된다.
